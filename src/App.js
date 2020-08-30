08a19cda69404f19d1d93764fc9c467dba29d651
Goldenbwuoy <mumanikidzwag@gmail.com>
2020-08-30 11:23:51 +0800

    dispatch setuser and settoken actions

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Login from './components/Login'
 import { getTokenFromUrl } from './config/spotify';
 import SpotifyWebApi from 'spotify-web-api-js'
 import Player from './components/Player'
+import {useDataLayerValue} from './DataLayer'
 
 const spotify = new SpotifyWebApi()
 
 function App() {
 
-  const [token, setToken] = useState(null)
+  const [{user, token}, dispatch] = useDataLayerValue() 
 
   useEffect(() => {
     const hash = getTokenFromUrl()
@@ -18,15 +19,26 @@ function App() {
     const _token = hash.access_token
 
     if(_token) {
-      setToken(_token)
+      
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token
+      })
 
       spotify.setAccessToken(_token)
 
       spotify.getMe().then(user => {
-        console.log("user: ", user)
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
       })
     }
+
   }, [])
+
+  console.log("user is: ", user)
+  console.log("token is: ", token)
 
   return (
     <div className="App">
--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,9 @@ export const initialState = {
     user: null,
     playlist: [],
     playing: false,
-    item: null
+    item: null,
+    //Remove after the development
+    token: 'BQDcDlp0-T6-dJ5iJ7FLuzZl1V3funMOX4_1LRhdKm10AysZ3BqDIqsG750am_DOEAE7J1Ahn1CPaLzDPlFWbYkCOI7medjZAUS_WiXUPugF-onk5x0phP1dRIK38P2cAbNWlbE2d3QW7PuWZGIUwQMMtqR_CGWWTreB3JqGJHGMshx2'
 }
 
 const reducer = (state, action) => {
@@ -15,6 +17,12 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 user: action.user
+            }
+
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
             }
     
         default:
